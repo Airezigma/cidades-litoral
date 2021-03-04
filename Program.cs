a8d1ed28c68f9ed7aaf8584f356048e4b1f560d6
Airezigma <gabs.sfv@gmail.com>
2021-03-04 14:16:33 -0300

    cidades

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace Cidades_litoral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peruibe");
+            Console.WriteLine("Itanhaem");
+            Console.WriteLine("Bertioga");
+            Console.WriteLine();
+
+            Console.WriteLine("Mongagua");
+            Console.WriteLine("Praia Grande");
+
+        }
+    }
+
+}
